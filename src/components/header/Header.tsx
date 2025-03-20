30383d984895649591ddb232d70c70bde91bf318
Danila Guryanov <forgustust@gmail.com>
2025-03-20 12:04:26 +0300

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import classes from './header.module.scss';
 
 export const Header = () => {
-  return <div className='bg-[rgba(68, 94, 189, 1)]'>header</div>;
+  return <div className={classes.header}>header</div>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     content: [
-      './src/**/*.{html,js,jsx,ts,tsx}', // Убедитесь, что пути к файлам указаны правильно
+      './src/**/*.{html,js,jsx,ts,tsx,scss,sass}', // Убедитесь, что пути к файлам указаны правильно
     ],
     theme: {
       extend: {},
